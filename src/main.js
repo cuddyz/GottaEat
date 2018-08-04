468c280f7741e0fae2c6afd761b5f181f4a9baae
Cuddy <zcuddy23@gmail.com>
2018-08-04 00:49:21 -0500

    Analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from './App'
 import router from './router'
 import './styles/app.scss'
@@ -13,4 +14,9 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
+})
+
+Vue.use(VueAnalytics, {
+  id: 'UA-123361165-1',
+  checkDuplicatedScript: true
 })
